1f2d29be74b666fff26a35adbf08032c3c592d91
moritanuki <ilovfam12@gmail.com>
2024-06-11 14:37:18 +0900

    Loadingをコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+import Loading from './components/Loading.vue'
+
+const isLoading = ref(true)
+</script>
 <template>
   <div class="w-screen h-screen bg-gray">
-    <div class="loader relative inset-1/2 text-white"></div>
+    <Loading v-if="isLoading" />
   </div>
 </template>
-<style scoped>
-.loader,
-.loader:before,
-.loader:after {
-  position: absolute;
-  content: '';
-  width: 12px;
-  height: 12px;
-  border-radius: 100%;
-  animation: loader 1.8s infinite ease-in-out;
-}
-
-.loader:before {
-  left: -14px;
-  animation-delay: -0.32s;
-}
-
-.loader {
-  animation-delay: -0.16s;
-}
-
-.loader:after {
-  left: 14px;
-}
-
-@keyframes loader {
-
-  0%,
-  80%,
-  100% {
-    /* x軸 y軸(元々の要素より12px下) ぼかし 影サイズの拡張(マイナスにすればするほど影サイズが小さくなる) */
-    box-shadow: 0 12px 0 -7px;
-  }
-
-  40% {
-    /* x軸 y軸(元々の要素より12px下) ぼかし 影サイズの拡張 */
-    box-shadow: 0 12px 0 0;
-  }
-}
-</style>
